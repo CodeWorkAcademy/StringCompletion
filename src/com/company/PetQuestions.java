c83d1478e93eb219ae1976d24e9b1f091d7bf823
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-24 10:19:38 -0400

    Initial Commit

---
+++ src/com/company/PetQuestions.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class PetQuestions {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name;
+        String breed;
+        int age;
+
+        System.out.print("Greetings. What is your pet's name?");
+        name = keyboard.next();
+
+        System.out.print("What kind of animal is "+name+"?");
+        System.out.print("How old is"+name+"?");
+
+        System.out.print(name+" is your "+breed+"and it is"+);
+    }
+}
